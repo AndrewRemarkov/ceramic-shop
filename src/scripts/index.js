67262dac7b04e0263b681781f3dbe1b05326429b
RemarkoWeb <remarkoweb@gmail.com>
2025-05-23 04:06:36 +0700

    feat(works): create and adapt carousel in works section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,33 @@
 import '/src/scss/style.scss';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  slidesPerView: 1,
+  spaceBetween: 5,
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.icon-right-open',
+    prevEl: '.icon-left-open'
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 5
+    },
+    1920: {
+      slidesPerView: 3,
+      spaceBetween: 35
+    }
+  }
+});
